a48ba2d3ecdb8b3fbad1fc776c4e1321a8347e27
novakvova <novakvova@gmail.com>
2018-09-26 16:19:36 +0300

    Good project

---
+++ LaptopWebSite/Controllers/TestCropperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaptopWebSite.Controllers
+{
+    public class TestCropperController : Controller
+    {
+        // GET: TestCropper
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
